642d60020e15ef14655205ad935a4c3a9bb71ef7
Morgan Creekmore <gamingrobot@gmail.com>
2015-10-03 19:49:13 -0500

    spock -> spockbot

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from spock import Client
+import logging
+
 from config import plugins, settings
+from spockbot import Client
+
+logger = logging.getLogger('spockbot')
+logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     client = Client(plugins=plugins, settings=settings)
